156d357d20c7c10f6b34ff879838504c2b7bee5e
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2023-09-09 21:17:17 -0400

    Simply settings to only include read/write keys

--- persair/config/settings.py
+++ persair/config/settings.py
@@ -2,14 +2,7 @@ import  os
 from    pydantic_settings  import BaseSettings
 
 class Keys(BaseSettings):
-    DBauthPath:str      = '/db/init.json'
-    ReadWriteKey:str    = "local"
+    ReadKey:str         = ""
+    WriteKey:str        = ""
 
-class Mongo(BaseSettings):
-    MD_URI:str          = "localhost:27017"
-    MD_DB:str           = "default"
-    MD_username:str     = "username"
-    MD_password:str     = "password"
-
-keys            = Keys()
-mongosettings   = Mongo()
+keys                    = Keys()
